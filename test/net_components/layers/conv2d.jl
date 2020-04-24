f01b562edf4beea66f6336806b02c110b5802af4
Samuele Marro <marrosamuele@gmail.com>
2020-04-24 20:21:54 +0200

    Minor fix.

--- test/net_components/layers/conv2d.jl
+++ test/net_components/layers/conv2d.jl
@@ -208,13 +208,14 @@ using MIPVerify: check_size, increment!
         input = reshape([1:1:25;], (1, 5, 5, 1))
         filter = ones(3, 3, 1, 1)  # basic tensor containing all ones
         bias = [0]
+        # Computed using Tensorflow
         true_output_raw = [
             63 72 81;
             108 117 126;
             153 162 171
         ]
         true_output = reshape(transpose(true_output_raw), (1, 3, 3, 1))
-        p = Conv2d(filter, bias, Conv2d.Padding.valid)
+        p = Conv2d(filter, bias, Padding.valid)
         @testset "Numerical Input, Numerical Layer Parameters" begin
             evaluated_output = MIPVerify.conv2d(input, p)
             @test evaluated_output == true_output
